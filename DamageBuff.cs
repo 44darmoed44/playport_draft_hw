a77a0cff78390e15233340670560dbcb13a7eec6
darmoed <144627432+44darmoed44@users.noreply.github.com>
2024-01-09 17:05:58 +0200

    Add files via upload

---
+++ DamageBuff.cs
@@ -0,0 +1,20 @@
+﻿using Battle.Actors;
+
+namespace Battle.Abilities
+{
+    public class DamageBuff : IAbility
+    {
+        public int DamageBuffValue;
+
+        public DamageBuff(int damageBuffValue)
+        {
+            DamageBuffValue = damageBuffValue;
+        }
+
+        public void Use(Actor target)
+        {
+            target.Damage += DamageBuffValue;
+            target.Log($"Using buff. Damage increased by {DamageBuffValue}");
+        }
+    }
+}
---
+++ HpBuff.cs
@@ -0,0 +1,20 @@
+﻿using Battle.Actors;
+
+namespace Battle.Abilities
+{ 
+    public class HpBuff : IAbility
+    {
+        public int HpBuffValue;
+
+        public HpBuff(int hpBuffValue)
+        {
+            HpBuffValue = hpBuffValue;
+        }
+
+        public void Use(Actor target)
+        {
+            target.Hp += HpBuffValue;
+            target.Log($"healing by {HpBuffValue} HP");
+        }
+    }
+}
